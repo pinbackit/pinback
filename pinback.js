a2513612669b66085a093e704350b7bf5c50f76a
Pinback <pinbackit@users.noreply.github.com>
2016-03-16 21:38:40 -0400

    Update pinback.js to v0.2
    no longer requires jQuery

--- pinback.js
+++ pinback.js
@@ -1,47 +1,52 @@
 (function(){
 
-  var version = '0.1'
+  var version = '0.2'
     , boards = {}
     , board = {}
+    , selected_board
     , pins = []
     , pin_count = 0
     , delay = 1000
     , timer;
-  
+
   // make sure user is logged in and on profile page
-  if (location.hostname.match(/pinterest.com$/) && $ && $('.usernameLink').length > 0) {
-    if ($('.boardLinkWrapper').length > 0) {
+  if (location.hostname.match(/pinterest.com$/) && document.querySelector('.usernameLink')) {
+    if (document.querySelector('.boardLinkWrapper')) {
       // already on profile page
       start();
     } else {
       // send to profile page
-      var profile_link = ($('.thumbUserInfo a, .BoardCount').length > 0) ? $('.thumbUserInfo a, .BoardCount') : $('.usernameLink');
-      var selected_board = location.pathname;
-      $(document).bind('DOMNodeInserted', function(e) {
-        // wait for boards to be loaded
-        if ($(e.target).find('.boardLinkWrapper').length > 0) {
-          $(document).unbind('DOMNodeInserted');
-          start(selected_board);
-        }
-      });
+      var profile_link = document.querySelector('.profileSource, .BoardCount, .usernameLink');
+      selected_board = location.href;
+      document.addEventListener('DOMNodeInserted', listenForProfile);
       profile_link.click();
     }
   } else {
     alert('Log in and visit your profile to start (pinterest.com/username)');
     return false;
   }
-  
+
+  // wait for profile to load
+  function listenForProfile() {
+    if (document.querySelector('.boardLinkWrapper')) {
+      document.removeEventListener('DOMNodeInserted', listenForProfile);
+      start();
+    }
+  }
+
   // show overlay
-  function start(selected_board) {
-  
+  function start() {
+
     // check if overlay is already open
-    if ($('#pboverlay').length > 0) return false;
-  
+    if (document.querySelector('#pboverlay')) return false;
+
     // start at top of page
     window.scrollTo(0,0);
-    
-    $('body').append('\
-<div id="pboverlay">\
+
+    // create overlay
+    var overlay = document.createElement('div');
+    overlay.id = 'pboverlay';
+    overlay.innerHTML = '\
 <style>\
   #pboverlay { display: block; bottom: 0; left: 0; right: 0; top: 0; z-index: 9999; position: fixed; background: rgba(0, 0, 0, 0.8); color: white; text-align: center; }\
   #pboverlay .close { color: white; position: absolute; top:10px; right:20px; font-size: 30px; }\
@@ -61,56 +66,74 @@
     </button>\
   </p>\
   <p><a href="http://www.pinback.it">Pinback v'+version+'</a></p>\
-</form>\
-</div>');
-  
-    var select = $('#pboverlay select');
-    select.append($("<option>", {
-      text: ' - All public pins - ',
-      value: '/' + location.pathname.split('/')[1] + '/pins/',
-      data: {name: 'all public pins'}
-    }));
-    
-    $('.boardLinkWrapper').each(function() {
-      var name = $.trim($(this).find('.boardName').text());
-      select.append($("<option>", {
-        text: name,
-        value: $(this).attr('href'),
-        data: {name: name},
-        selected: (selected_board == $(this).attr('href'))
-      }));
+</form>';
+
+    // add overlay to document
+    document.querySelector('body').appendChild(overlay);
+
+    // find boards
+    var select = document.querySelector('#pboverlay select');
+    var option = document.createElement('option');
+    option.text = ' - All public pins - ';
+    option.value = '/' + location.pathname.split('/')[1] + '/pins/';
+    option.dataset.name = 'all public pins';
+    select.add(option);
+
+    Array.prototype.forEach.call(document.querySelectorAll('.boardLinkWrapper'), function(el, i) {
+      var name = el.querySelector('.name').innerText;
+      var option = document.createElement('option');
+      option.text = name;
+      option.value = el.getAttribute('href');
+      option.dataset.name = name;
+      option.selected = (selected_board == el.href);
+      select.add(option);
     });
-    
-    $('#pboverlay button').on('click', function(e) {
-      e.preventDefault();
-      var name = select.find('option:selected').data('name');
-      $('a[href="'+select.val()+'"]').attr('href',select.val()+'?'+Date.now()).click();
+
+    document.querySelector('#pboverlay button').onclick = function() {
+      var selected = select.querySelector('option:checked');
+      var name = selected.dataset.name;
+
+      var link = document.querySelector('a[href="'+selected.value+'"]');
+      link.href = selected.value+'?'+Date.now();
+      link.click();
+
       status('Exporting ' + name + '...');
-      $('#pboverlay .controls').html('<meter min="0" max="100"></meter>');
-    });
-      
-    $('#pboverlay .close').on('click', function(e) {
-      e.preventDefault();
+      document.querySelector('#pboverlay .controls').innerHTML = '<meter min="0" max="100"></meter>';
+
+      return false;
+    };
+
+    document.querySelector('#pboverlay .close').onclick = function() {
       window.scrollTo(0,0);
       location.href = location.pathname;
-    });
-    
+      return false;
+    };
+
     // watch for ajax
-    $(document).ajaxComplete(function(event, xhr, settings) {
-      if (!xhr.responseJSON) return false;
-      // regular resources
-      if (xhr.responseJSON.resource && xhr.responseJSON.resource_response && xhr.responseJSON.resource_response.data) {
-        parse_resource(xhr.responseJSON.resource, xhr.responseJSON.resource_response.data);
-      }
-      // cached resources
-      else if (xhr.responseJSON.resource_data_cache) {
-        $.each(xhr.responseJSON.resource_data_cache, function() {
-          if (this.resource) parse_resource(this.resource, this.data);
-        });
-      }
-    });
+    XMLHttpRequest.prototype.send = (function(orig){
+      return function(){
+
+        this.addEventListener('loadend', function(e){
+          if (e.target.getResponseHeader('content-type').match(/json/)) {
+            var json = JSON.parse(e.target.response);
+            // regular resources
+            if (json.resource && json.resource_response && json.resource_response.data) {
+              parse_resource(json.resource, json.resource_response.data);
+            }
+            // cached resources
+            else if (json.resource_data_cache) {
+              json.resource_data_cache.forEach(function(el, i) {
+                if (el.resource) parse_resource(el.resource, el.data);
+              });
+            }
+          }
+        }, false);
+
+        return orig.apply(this, arguments);
+      };
+    })(XMLHttpRequest.prototype.send);
   }
-  
+
   // parse incoming ajax responses
   function parse_resource(r, data) {
     if (r.name == 'UserResource' || r.name == 'BoardResource') {
@@ -118,36 +141,36 @@
       pins = [];
     }
     if (r.name == 'UserPinsResource' || r.name == 'BoardFeedResource') {
-      $.each(data, function() {
-        if (this.id) {
+      data.forEach(function(p, i) {
+        if (p.type == 'pin') {
           // add board if not already
-          if (!boards[this.board.name]) boards[this.board.name] = {
-            id: this.board.id,
-            name: this.board.name,
-            url: "https://www.pinterest.com"+this.board.url,
-            privacy: this.board.privacy,
-            pins: []
+          if (!boards[p.board.name]) boards[p.board.name] = {
+            id:           p.board.id,
+            name:         p.board.name,
+            url:          "https://www.pinterest.com"+p.board.url,
+            privacy:      p.board.privacy,
+            pins:         []
           }
           // add pin to board
-          boards[this.board.name].pins.push({
-            id: this.id,
-            link: this.link,
-            description: $.trim(this.description),
-            url: "https://www.pinterest.com/pin/"+this.id,
-            image: this.images.orig.url,
-            color: this.dominant_color,
-            longitude: (this.place && this.place.longitude) || null,
-            latitude: (this.place && this.place.latitude) || null,
-            pinner: this.pinner.username,
-            privacy: this.privacy,
-            date: Date.parse(this.created_at)
+          boards[p.board.name].pins.push({
+            id:           p.id,
+            link:         p.link,
+            description:  p.description,
+            url:          "https://www.pinterest.com/pin/"+p.id,
+            image:        p.images.orig.url,
+            color:        p.dominant_color,
+            longitude:    (p.place && p.place.longitude) || null,
+            latitude:     (p.place && p.place.latitude) || null,
+            pinner:       p.pinner.username,
+            privacy:      p.privacy,
+            date:         Date.parse(p.created_at)
           });
           pin_count++;
         }
       });
-      
+
       progress(pin_count, board.pin_count);
-      
+
       // scroll page to find more pins
       var next = (typeof(r.options.bookmarks) !== "undefined") ? r.options.bookmarks[0] : null;
       if (timer) clearTimeout(timer);
@@ -157,71 +180,72 @@
       } else {
         // keep going
         timer = setTimeout(function() {
-          $("html, body").animate({ scrollTop: document.body.scrollHeight }, delay);
+          window.scrollTo(0, document.body.scrollHeight);
         }, delay);
       }
     }
   }
-  
+
   function status(s) {
-    $('#pboverlay h1').text(s);
+    document.querySelector('#pboverlay h1').innerText = s;
   }
-  
+
   function progress(a, b) {
     status('Exporting ' + a + " of " + b + '...');
-    $('#pboverlay meter').val((a/b)*100);
+    document.querySelector('#pboverlay meter').value = (a/b)*100;
   }
-  
+
   function privacy(p) {
     // borrowed PRIVATE attribute format from Delicious
     return (p != 'public') ? 1 : 0;
   }
-  
+
   function escapeHtml(unsafe) {
     return unsafe.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g, "&#039;");
   }
-  
+
   // generate Netscape bookmark file
   function done() {
-    
+
     status('Export complete!');
 
-    // template header 
+    // template header
     var data = '<!DOCTYPE NETSCAPE-Bookmark-file-1>\n\
 <META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8">\n\
 <TITLE>Bookmarks</TITLE>\n\
 <H1>Bookmarks</H1>\n\
 <DL><p>\n';
-  
-    $.each(boards, function() {
+
+    for (var name in boards) {
+      var b = boards[name];
       // board
-      data += '<DT><H3 GUID="'+this.id+'" ORIGLINK="'+this.url+'" PRIVATE="'+privacy(this.privacy)+'">'+escapeHtml(this.name)+'</H3>\n<DL><p>\n';
-      
+      data += '<DT><H3 GUID="'+b.id+'" ORIGLINK="'+b.url+'" PRIVATE="'+privacy(b.privacy)+'">'+escapeHtml(b.name)+'</H3>\n<DL><p>\n';
+
       // pins
-      $.each(this.pins, function(i, p) {
+      b.pins.forEach(function(p, i) {
         data += '<DT><A HREF="'+(p.link||p.url)+'" GUID="'+p.id+'" ORIGLINK="'+p.url+'" IMAGE="'+p.image+'" COLOR="'+p.color+'" AUTHOR="'+p.pinner+'" PRIVATE="'+privacy(p.privacy)+'" ADD_DATE="'+p.date+'" LONGITUDE="'+(p.longitude||'')+'" LATITUDE="'+(p.latitude||'')+'">'+escapeHtml(p.description)+'</A>\n';
       });
-      
+
       // board footer
       data += '</DL><p>\n';
-    });
-  
+    }
+
     // template footer
     data += '</DL><p>';
-    
+
     // create file
     var filename = location.pathname.replace(/^\/|\/$/g, '').replace(/\//g,'-')+".html";
     var blob = new Blob([data], {type: 'text/html'});
     var url = URL.createObjectURL(blob);
-    
+
     // add save button
-    $('#pboverlay .controls').html('<a href="'+url+'" download="'+filename+'" class="Button btn rounded primary"><span class="buttonText">Save export file</span></a>');
-    
+    document.querySelector('#pboverlay .controls').innerHTML = '<a href="'+url+'" download="'+filename+'" class="Button btn rounded primary"><span class="buttonText">Save export file</span></a>';
+
     // add note for browsers that don't support download attribute
     if (typeof(document.createElement('a').download) === "undefined") {
-      $('#pboverlay .controls a').on('click', function() {
+      document.querySelector('#pboverlay .controls a').onclick = function() {
         alert('Choose File > Save As in your browser to save a copy of your export');
-      });
+      }
     }
   }
 
